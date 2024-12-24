b802174bf6e7537a0139bbdb1ff5a9322e8bf5b2
Callmeteddyfx <136664394+Callmeteddyfx@users.noreply.github.com>
2024-12-23 17:24:39 -0800

    Share button and refresh quotes working

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 
 function getQuote() {
-    fetch("https://zenquotes.io/api/random")
+    fetch("https://api.adviceslip.com/advice")
     .then(response => {
-        console.log(response);
-       return response.json();
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+            return response.json();
     })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+        
+    .then(data => {
+        console.log(data)
+        let advice = data.slip.advice;
+        document.getElementById('text').innerText = `"${advice}"`;
+
+        const p = document.getElementById('text');
+    p.style.margin = '20px'; 
+    p.style.fontFamily = 'Poppins, serif';
+    p.style.fontSize = '20px';
+    p.style.overflowWrap = 'break-word';
+    p.style.justifyContent = 'center';
+    p.style.alignItems = 'center';
+    });
+
 }
 
-    
+window.onload = getQuote;
+
+function shareQuote(){
+   const textToShare = document.getElementById('text').textContent;
+
+   if (navigator.share) {
+    navigator.share({
+        title: 'Share Text',
+        text: textToShare,
+    }).then(() => {
+        console.log('Successfully shared');
+    }).catch((error) => {
+        console.error('Error sharing: ', error);
+    });
+} else {
+    alert('Web Share API is not supported on this device.');
+    }
+}
+
